69553241ace5b85fb236dd7aac68c598c4665881
Diego <dmalmeida@gmail.com>
2023-10-30 20:41:25 -0500

    first-commit

---
+++ src/test/java/ec/com/orion/learning/springboot/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.orion.learning.springboot.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
